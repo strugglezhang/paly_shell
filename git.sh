ba0c169821ca847607c37825cb445374ba104e74
zhangchao12 <zhangchao12@tal.com>
2021-04-14 11:04:04 +0800

    优化功能

--- git.sh
+++ git.sh
@@ -5,11 +5,16 @@ param3=$3
 param4=$4
 param5=$5
 
+# 获取当前分支
 cur_branch=`git branch |grep "*" | awk -F " " '{print $2}'`;
-
+# 提示输出
+tipResult="Success !";
+# 最终tag名称
+finallyTag="";
 
 # push操作
 gpush(){
+    gpull
     if [ -z "$param2" ]  ;then
         (git push origin $cur_branch)
     else
@@ -44,7 +49,6 @@ gm(){
 # log 操作
 glog(){
     (git log)
-
 }
 
 # add 操作
@@ -61,16 +65,136 @@ gcm(){
     (git commit -m  "$param2")
 }
 
+# 展示近期的10个tag
 gtag(){
-    (git tag -l)
+    (git tag --sort=-v:refname | head -n 10)
 }
 
+
+# 获取修改状态
 gst(){
      (git status)
 }
 
+# 自动打pre-master上面的tag并且提交
+gpt() {
+   if [[ "$cur_branch" != "pre-master" ]]  ;then
+      tipResult="Branch is no support !!!";
+      echo  $tipResult;
+      exit;
+   fi
+   gp=`git pull`;
+   gtag=`git tag --sort=-v:refname | head -n 1`;
+   versionArray=()
+   preVersion=(${gtag//\-/ })
+   for i in "${!preVersion[@]}"; do
+      if [[ $i == 0 ]] ; then
+          versionArray=(${preVersion[i]//\./ })
+      fi
+   done
+   # 获取不到tag,现场退出
+   if [ "${#versionArray[@]}" -eq 0 ] ; then
+      tipResult="Tag is not found !!!";
+      echo  $tipResult;
+      exit;
+   fi
+
+   last=`expr ${versionArray[2]} + 1 `;
+   mid=${versionArray[1]};
+   first=${versionArray[0]}
+   if [ $last -ge "99" ] ; then
+       mid=`expr $mid + 1 `;
+       last=0;
+   fi
+
+   # 中间数字超过99第一位数据相加
+   if [ $mid -ge "99" ] ; then
+      mid=0;
+      fv=`expr ${first:1:1} + 1`;
+      first="${first:0:1}${fv}";
+   fi
+   tagName="$first.$mid.$last-$cur_branch";
+   finallyTag=$tagName;
+   # 执行确认工作
+   f=`confirm`
+   if [ $? -ne 1 ] ; then
+      tipResult="Tag is not need confirm !!!";
+      echo  $tipResult;
+      exit;
+   fi
+
+   (git tag $tagName -m "$param2")
+   (git push origin $finallyTag)
+   echo  $tipResult;
+   exit;
+}
+
+
+# 基础星号输出
+printBasic(){
+  for (( j = 0; j <= $1; j++ )) ; do
+      str="${str}*";
+  done
+  printf "${str}\n";
+}
+
+# 补充空格输出
+printInfo(){
+  out=$(random 31 37);
+  prefix="****"
+  suffix="****"
+  strNum="${prefix}${2}${3}";
+  echoStr="${prefix}${2}\033[${out}m${3}\033[0m";
+  supplement=`expr ${1} - ${#strNum} - ${#suffix}`;
+   
+  k=" ";
+  for (( i = 0; i <= $supplement; i++ )); do
+    echoStr="${echoStr}${k}";
+  done
+  echo "${echoStr}${suffix}";
+}
+
+# 确定是否提交
+confirm(){
+  while true
+  do
+	  read -r -p "Push tag to origin? [Y/n] " input
+  	case $input in
+	    [yY][eE][sS]|[yY])
+			echo "Yes"
+			return 1;
+			;;
+
+	    [nN][oO]|[nN])
+			echo "No"
+			return 2;
+			;;
+
+	    *)
+			echo "Invalid input..."
+			return 3;
+			;;
+	  esac
+  done
+}
+
+# 获取随机数
+function random()
+{
+    min=$1;
+    max=$2-$1;
+    num=$(date +%s+%N);
+    ((retnum=num%max+min));
+    #进行求余数运算即可
+    echo $retnum;
+    #这里通过echo 打印出来值，然后获得函数的，stdout就可以获得值
+    #还有一种返回，定义全价变量，然后函数改下内容，外面读取
+}
+
 # 接受命令行参数
+# currTime=$(date "+%Y-%m-%d %H:%M:%S")
 currTime=$(date +"%Y-%m-%d %T")
+
 if [[ "$cmd" == "" ]] ;then
     echo "********************************************"
     echo "********************************************"
@@ -92,23 +216,39 @@ if [ ! -d "${PWD}/.git" ]; then
     exit;
 fi
 
-# 获取分支名称
-printf '********************************************************\n';
-printf '********************************************************\n';
-printf '**** Show Branch And Date                           ****\n';
-
-# 假装是美化了输出
-echo "**** Date $currTime                       ****"
-prePrint="**** @Current Branch ";
-midPrint="${cur_branch}   ";
-printf "${prePrint}";
-printf $midPrint
-total=`expr 56 - ${#midPrint} - ${#prePrint}`
-for((i=1;i<$total;i++))
-do
-    printf " ";
+row=8
+line=56;
+for((i=1;i<=$row;i++)); do
+  str=""
+  case $i in
+  1)
+    (printBasic ${line})
+    ;;
+  2)
+    (printBasic ${line})
+    ;;
+  3)
+    (printInfo ${line} "  @Show Same Information !" "")
+    ;;
+  4)
+    (printInfo ${line} "  @Date " "$currTime")
+    ;;
+  5)
+    (printInfo ${line} "  @Branch " "( ${cur_branch} )")
+    ;;
+  6)
+    #(printInfo ${line} "  @Result " "${tipResult}")
+    ;;
+  7)
+    (printBasic ${line})
+    ;;
+  8)
+    (printBasic ${line})
+    ;;
+  esac
 done
-printf "****\n"
-printf "********************************************************\n"
-printf "********************************************************\n"
+
 ($cmd)
+
+
+
